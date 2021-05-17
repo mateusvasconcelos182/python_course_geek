87fbf5a1aba55c672f08cdab92630a835c0fd22d
Mateus Vasconcelos <mateusvasconcelos.araujo@gmail.com>
2021-05-17 20:48:07 -0300

    criado pasta organização, movido arquivos para pasta correta

---
+++ 07_Coleções_Py/40.deque.py
@@ -0,0 +1,15 @@
+"""
+Módulo Collectionis - Deque
+
+Podemos dizer que o deque é uma lista de alta performance.
+"""
+
+from collections import deque
+
+deq = deque('geek')
+
+print(deq)
+
+
+deq.append('y')
+
