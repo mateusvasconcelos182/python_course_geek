ff66d705dcd5f1cc883ce99298354675a7fff587
Mateus Vasconcelos <mateusvasconcelos.araujo@gmail.com>
2021-05-18 22:42:05 -0300

    modificado

--- 08_Funções_Py/43.Definindo_funções.py
+++ 08_Funções_Py/43.Definindo_funções.py
@@ -1,5 +1,6 @@
 """
 Definindo Funções
+
 Ex: print(), len(), min(), max(), count;
 - Funções são pequenos trechos de código que realizam tarefas específicas;
 - Pode ou não receber entradas de dados e retornar uma saída de dados;
@@ -8,6 +9,8 @@ Ex: print(), len(), min(), max(), count;
 OBS: Se você escrever uma função que realiza várias tarefas dentro dela,
 é bom fazer uma verificação para que a função seja simplificada;
 """
+
+
 #Exemplo de utilização funções:
 
 cores = ['verde', 'amarelo', 'azul', 'branco']
@@ -15,3 +18,7 @@ cores = ['verde', 'amarelo', 'azul', 'branco']
 #Utilizando a função integrada (Built-in) do Python print()
 
 print(cores)
+
+
+
+
